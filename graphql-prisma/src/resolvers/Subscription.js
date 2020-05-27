b8adaf396e144e00eb04bcd908e39cd6d1ce0612
nathanjwtx <nathanjwtx@nathanjwtx.io>
2020-05-27 08:42:31 -0500

    refactored subscriptions to use prisma

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -15,19 +15,27 @@ const Subscription = {
 	},
 	comment: {
 		// comments on a specific post id
-		subscribe(parent, { postId }, { db, pubsub }, info) {
-			const post = db.posts.find((post) => post.id === postId && post.published)
-
-			if (!post) {
-				throw new Error('Comment subscription: comment not found')
-			}
-
-			return pubsub.asyncIterator(`comment ${postId}`)
+		subscribe(parent, { postId }, { prisma }, info) {
+			return prisma.subscription.comment({
+				where: {
+					node: {
+						post: {
+							id: postId
+						}
+					}
+				}
+			}, info)
 		}
 	},
 	post: {
-		subscribe(parent, args, { pubsub }, info) {
-			return pubsub.asyncIterator('post')
+		subscribe(parent, args, { prisma }, info) {
+			return prisma.subscription.post({
+				where: {
+					node: {
+						published: true
+					}
+				}
+			}, info)
 		}
 	}
 }
