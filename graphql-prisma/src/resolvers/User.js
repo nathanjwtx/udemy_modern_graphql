8dc969e4b32385d8906b26e150860b8c586af910
nathanjwtx <nathanjwtx@nathanjwtx.io>
2020-06-09 08:22:11 -0500

    continued authentication of User

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,18 @@
 /* eslint-disable no-unused-vars */
+
+import { getUserId } from '../utils/getUserId'
+
 const User = {
+	email(parent, args, { request }, info) {
+		const userId = getUserId(request, false)
+
+		// id needs to be part of the request for this to work
+		if (userId && userId === parent.id) {
+			return parent.email
+		} else {
+			return null
+		}
+	}
 }
 
 export { User }
